955daffd77d5b0a6ebd61505237866badb9352aa
Minu Kim <kmw106933@naver.com>
2025-06-29 22:26:52 +0900

    inital Commit

---
+++ src/main/java/backend/glloserver/GllOServerApplication.java
@@ -0,0 +1,13 @@
+package backend.glloserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GllOServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GllOServerApplication.class, args);
+    }
+
+}
