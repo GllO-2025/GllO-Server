ac26daf1b6efe7c449312dde3a6eec2850f14503
minsikk <131092169+1224kang@users.noreply.github.com>
2025-07-17 11:50:28 +0900

    [feat] 애플 소셜 로그인 api + 테스트용 리디렉션 api 추가

--- src/main/java/backend/glloserver/auth/controller/AuthController.java
+++ src/main/java/backend/glloserver/auth/controller/AuthController.java
@@ -1,6 +1,7 @@
 package backend.glloserver.auth.controller;
 
 import backend.glloserver.auth.service.AuthService;
+import backend.glloserver.auth.service.dto.ios.AppleLoginRequest;
 import backend.glloserver.auth.service.dto.AuthInfoDto;
 import backend.glloserver.auth.service.dto.GoogleLoginRequest;
 import backend.glloserver.auth.service.dto.LoginResponse;
@@ -9,7 +10,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,5 +28,24 @@ public class AuthController {
         AuthInfoDto authInfo = authService.googleLogin(request);
         LoginResponse response = new LoginResponse(authInfo.authMember());
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/auth/login/apple")
+    public ResponseEntity<LoginResponse> appleLogin(@RequestBody AppleLoginRequest request){
+        AuthInfoDto authInfo = authService.appleLogin(request);
+        LoginResponse response = new LoginResponse(authInfo.authMember());
+        return ResponseEntity.ok(response);
+    }
+
+    //리디렉션용
+    @PostMapping("/login/oauth2/code/apple")
+    public ResponseEntity<String> handleApppleCallback(@RequestParam Map<String,String> params){
+        String code=params.get("code"); //authorization_code
+        String idToken=params.get("id_token"); //id_token
+
+        System.out.println("code: "+code);
+        System.out.println("idToken: "+idToken);
+
+        return ResponseEntity.ok("Apple callback received");
     }
 }
