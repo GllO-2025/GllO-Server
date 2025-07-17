d613be3de333f7c548fe1ec0da44b3f861ce81b6
minsikk <131092169+1224kang@users.noreply.github.com>
2025-07-17 11:51:30 +0900

    [feat] 애플 커스텀 에러 코드 추가

--- src/main/java/backend/glloserver/auth/exception/AuthErrorCode.java
+++ src/main/java/backend/glloserver/auth/exception/AuthErrorCode.java
@@ -15,8 +15,14 @@ public enum AuthErrorCode implements ErrorResponse {
     EXPIRED_REFRESH_TOKEN(HttpStatus.FORBIDDEN, "만료된 토큰입니다."),
     INVALID_PASSWORD(HttpStatus.NOT_FOUND, "가입하지 않은 회원입니다."),
     DUPLICATED_MEMBER(HttpStatus.CONFLICT, "이미 가입한 회원입니다."),
-    KAKAO_LOGIN_INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "구글 로그인에 실패했습니다."),
+    GOOGLE_LOGIN_INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "구글 로그인에 실패했습니다."),
+    APPLE_LOGIN_INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "애플 로그인에 실패했습니다."),
     EXPIRED_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
+    INVALILD_IDTOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 id token 입니다."),
+    INVALID_IDTOKEN_ISS(HttpStatus.UNAUTHORIZED, "TOKEN ID의 iss가 유효하지 않습니다."),
+    INVALID_IDTOKEN_AUDIENCE(HttpStatus.UNAUTHORIZED, "TOKEN ID의 aud가 유효하지 않습니다."),
+    INVALID_IDTOKEN_SIGNATURE(HttpStatus.UNAUTHORIZED, "TOKEN ID의 signature가 유효하지 않습니다."),
+    IDTOKEN_PARSING_ERROR(HttpStatus.BAD_REQUEST,"TOKEN ID 파싱에 실패했습니다."),
     REFRESH_REUSE_EXCEPTION(HttpStatus.UNAUTHORIZED, "이미 사용한 토큰입니다. 다시 로그인 해주세요.");
 
     private final HttpStatus status;
