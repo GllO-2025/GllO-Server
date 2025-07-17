c06e798f01ead2aa39f57238c22053b904626588
minsikk <131092169+1224kang@users.noreply.github.com>
2025-07-17 11:48:41 +0900

    [feat] ApplePublicKeyProvider 추가

--- src/main/java/backend/glloserver/auth/config/AuthClientConfig.java
+++ src/main/java/backend/glloserver/auth/config/AuthClientConfig.java
@@ -1,16 +1,14 @@
 package backend.glloserver.auth.config;
 
+import backend.glloserver.auth.service.ApplePublicKeyProvider;
 import backend.glloserver.auth.service.AuthClient;
 import backend.glloserver.auth.service.client.DevAuthClient;
 import backend.glloserver.auth.service.client.ProdAuthClient;
 import lombok.extern.slf4j.Slf4j;
 
-
-import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -19,18 +17,19 @@ public class AuthClientConfig {
 
     @Bean
     @Profile("prod")
-    public AuthClient prodAuthClient(){
-        return new ProdAuthClient(createRestClient());
+    public AuthClient prodAuthClient(ApplePublicKeyProvider applePublicKeyProvider){
+        return new ProdAuthClient(createRestClient(), applePublicKeyProvider);
     }
 
     @Bean
     @Profile("default")
-    public AuthClient devAuthClient(){
+    public AuthClient devAuthClient(ApplePublicKeyProvider applePublicKeyProvider){
         log.warn("테스트 인증 환경 설정.");
-        return new DevAuthClient(createRestClient());
+        return new DevAuthClient(createRestClient(),applePublicKeyProvider);
     }
 
-    private RestClient createRestClient() {
+    @Bean
+    public RestClient createRestClient() {
         return RestClient.builder()
                 .build();
     }
