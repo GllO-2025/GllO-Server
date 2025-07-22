bf3b7d9e5f53de6f27e9daba880ee7010fe47d83
Minu Kim <kmw106933@naver.com>
2025-07-23 01:43:22 +0900

    chore: 헬스 체크를 위한 컨트롤러 생성

---
+++ src/main/java/backend/glloserver/logging/controller/HealthCheckController.java
@@ -0,0 +1,16 @@
+package backend.glloserver.logging.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health-check")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("ok");
+    }
+}
